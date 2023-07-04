907569f9e9236d5c0ed042de0110303354d634b6
Tim Fischer <tfischer@TnM.inc>
2023-07-04 08:30:40 -0500

    launch_config and ASG

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_instance" "example" {
-  ami           =  "ami-0fb653ca2d3203ac1" 
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+}
 
-  user_data_replace_on_change = true
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name  min_size = 2
+  max_size = 10
 
-  tags = {
-    Name = "terraform-example"
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -36,7 +41,3 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
